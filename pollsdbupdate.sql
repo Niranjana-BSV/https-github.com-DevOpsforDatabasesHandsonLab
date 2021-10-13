c49e6ee4e348977ad93ff66d6058d4f05eaff550
Niranjana-BSV <77824251+Niranjana-BSV@users.noreply.github.com>
2021-10-13 12:50:39 +0530

    Update pollsdbupdate.sql

--- pollsdbupdate.sql
+++ pollsdbupdate.sql
@@ -1,4 +1,4 @@
 INSERT INTO polls_question (id, question_text, pub_date) VALUES (2, 'how was pyhton code updation ', now());
-INSERT INTO polls_choice (id, choice_text, votes, question_id) VALUES (4, 'Better', 0, 2);
-INSERT INTO polls_choice (id, choice_text, votes, question_id) VALUES (5, 'Easy', 0, 2);
-INSERT INTO polls_choice (id, choice_text, votes, question_id) VALUES (6, 'difficult', 0, 2);
+INSERT INTO polls_choice (id, choice_text, votes, question_id) VALUES (5, 'Ok', 0, 2);
+INSERT INTO polls_choice (id, choice_text, votes, question_id) VALUES (6, 'Good', 0, 2);
+INSERT INTO polls_choice (id, choice_text, votes, question_id) VALUES (7, 'Excellent', 0, 2);
